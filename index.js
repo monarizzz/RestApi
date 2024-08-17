63e9d5b5692b861ffa1859571e819c7a3c2af7e4
Cloud <monaniza@gmail.com>
2024-08-18 01:28:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
+  
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
