fe3357c0d23a2e20cfd0023c3baf915755c78a4c
Cloud <monaniza@gmail.com>
2024-09-23 14:20:37 +0900

    P97

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello, World!' });
-  });
-  
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
